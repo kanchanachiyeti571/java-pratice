a49c5efdaae14a5459ea05af2807d36008ba581b
kanchanachiyeti571 <kanchanachiyeti333@gmail.com>
2026-01-26 12:18:34 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,16 @@
+package sweety;
+
+public class Demo{
+
+	public static void main(String[]args) {
+		int choice;
+		do {
+			System.out.println("1.Deposit");
+			System.out.println("2.Withdraw");
+			System.out.println("3.Exit");
+			choice = 3;
+		}while (choice !=3);
+	}
+}
+
+
